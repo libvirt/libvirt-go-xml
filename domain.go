9fc7b7b52085672dd73da7e0a3b69d67feb0fcc9
Daniel P. Berrange <berrange@redhat.com>
2016-12-16 11:23:51 +0000

    Initial import of code
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

---
+++ domain.go
@@ -0,0 +1,112 @@
+/*
+ * This file is part of the libvirt-go-xml project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ */
+
+package libvirtxml
+
+import (
+	"encoding/xml"
+)
+
+type DomainController struct {
+	Type  string `xml:"type,attr"`
+	Index string `xml:"index,attr"`
+}
+
+type DomainDiskFileSource struct {
+	File string `xml:"file,attr"`
+}
+
+type DomainDiskDriver struct {
+	Name string `xml:"name,attr"`
+	Type string `xml:"type,attr"`
+}
+
+type DomainDisk struct {
+	Type       string               `xml:"type,attr"`
+	Device     string               `xml:"device,attr"`
+	Driver     DomainDiskDriver     `xml:"driver"`
+	FileSource DomainDiskFileSource `xml:"source"`
+}
+
+type DomainInterfaceMAC struct {
+	Address string `xml:"address,attr"`
+}
+
+type DomainInterfaceModel struct {
+	Type string `xml:type,attr"`
+}
+
+type DomainInterface struct {
+	Type  string               `xml:"type,attr"`
+	MAC   string               `xml:"mac"`
+	Model DomainInterfaceModel `xml:"model"`
+}
+
+type DomainChardev struct {
+	Type string `xml:"type,attr"`
+}
+
+type DomainInput struct {
+	Type string `xml:"type,attr"`
+	Bus  string `xml:"bus,attr"`
+}
+
+type DomainGraphic struct {
+	Type string `xml:"type,attr"`
+}
+
+type DomainVideoModel struct {
+	Type string `xml:"type,attr"`
+}
+
+type DomainVideo struct {
+	Model DomainVideoModel `xml:"model"`
+}
+
+type DomainDeviceList struct {
+	Controllers []DomainController `xml:"controller"`
+	Disks       []DomainDisk       `xml:"disk"`
+	Interfaces  []DomainInterface  `xml:"interface"`
+	Serials     []DomainChardev    `xml:"serial"`
+	Consoles    []DomainChardev    `xml:"console"`
+	Inputs      []DomainInput      `xml:"input"`
+	Graphics    []DomainGraphic    `xml:"graphics"`
+	Videos      []DomainVideo      `xml:"video"`
+}
+
+type DomainMemory struct {
+	Value string `xml:",chardata"`
+	Unit  string `xml:"unit,attr"`
+}
+
+type Domain struct {
+	XMLName       xml.Name          `xml:"domain"`
+	Type          string            `xml:"type,attr"`
+	Name          string            `xml:"name"`
+	UUID          *string           `xml:"uuid"`
+	Memory        *DomainMemory     `xml:"memory"`
+	CurrentMemory *DomainMemory     `xml:"currentMemory"`
+	Devices       *DomainDeviceList `xml:"devices"`
+}
---
+++ domain_test.go
@@ -0,0 +1,139 @@
+/*
+ * This file is part of the libvirt-go-xml project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ */
+
+package libvirtxml
+
+import (
+	"encoding/xml"
+	"strings"
+	"testing"
+)
+
+var testData = []struct {
+	Object   *Domain
+	Expected []string
+}{
+	{
+		Object: &Domain{
+			Type: "kvm",
+			Name: "test",
+		},
+		Expected: []string{
+			`<domain type="kvm">`,
+			`  <name>test</name>`,
+			`</domain>`,
+		},
+	},
+	{
+		Object: &Domain{
+			Type: "kvm",
+			Name: "test",
+			Devices: &DomainDeviceList{
+				Disks: []DomainDisk{
+					DomainDisk{
+						Type:   "file",
+						Device: "cdrom",
+						Driver: DomainDiskDriver{
+							Name: "qemu",
+							Type: "qcow2",
+						},
+						FileSource: DomainDiskFileSource{
+							File: "/var/lib/libvirt/images/demo.qcow2",
+						},
+					},
+				},
+			},
+		},
+		Expected: []string{
+			`<domain type="kvm">`,
+			`  <name>test</name>`,
+			`  <devices>`,
+			`    <disk type="file" device="cdrom">`,
+			`      <driver name="qemu" type="qcow2"></driver>`,
+			`      <source file="/var/lib/libvirt/images/demo.qcow2"></source>`,
+			`    </disk>`,
+			`  </devices>`,
+			`</domain>`,
+		},
+	},
+	{
+		Object: &Domain{
+			Type: "kvm",
+			Name: "test",
+			Devices: &DomainDeviceList{
+				Inputs: []DomainInput{
+					DomainInput{
+						Type: "tablet",
+						Bus:  "usb",
+					},
+					DomainInput{
+						Type: "keyboard",
+						Bus:  "ps2",
+					},
+				},
+				Videos: []DomainVideo{
+					DomainVideo{
+						Model: DomainVideoModel{
+							Type: "cirrus",
+						},
+					},
+				},
+				Graphics: []DomainGraphic{
+					DomainGraphic{
+						Type: "vnc",
+					},
+				},
+			},
+		},
+		Expected: []string{
+			`<domain type="kvm">`,
+			`  <name>test</name>`,
+			`  <devices>`,
+			`    <input type="tablet" bus="usb"></input>`,
+			`    <input type="keyboard" bus="ps2"></input>`,
+			`    <graphics type="vnc"></graphics>`,
+			`    <video>`,
+			`      <model type="cirrus"></model>`,
+			`    </video>`,
+			`  </devices>`,
+			`</domain>`,
+		},
+	},
+}
+
+func TestDomain(t *testing.T) {
+	for _, test := range testData {
+		doc, err := xml.MarshalIndent(test.Object, "", "  ")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expect := strings.Join(test.Expected, "\n")
+
+		if string(doc) != expect {
+			t.Fatal("Bad xml:\n", string(doc), "\n does not match\n", expect, "\n")
+		}
+	}
+}
