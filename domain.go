2bf987a9cadff986df2451ffb890ade614744391
Daniel P. Berrangé <berrange@redhat.com>
2018-07-04 11:26:17 +0100

    Add HTM domain feature flag
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- domain.go
+++ domain.go
@@ -1939,6 +1939,7 @@ type DomainFeatureList struct {
 	SMM          *DomainFeatureSMM          `xml:"smm"`
 	IOAPIC       *DomainFeatureIOAPIC       `xml:"ioapic"`
 	HPT          *DomainFeatureHPT          `xml:"hpt"`
+	HTM          *DomainFeatureState        `xml:"htm"`
 	Capabilities *DomainFeatureCapabilities `xml:"capabilities"`
 	VMCoreInfo   *DomainFeatureState        `xml:"vmcoreinfo"`
 }
