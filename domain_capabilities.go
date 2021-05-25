ccb54365b4e3f97dee7e789d99cce755f82a9813
Daniel P. Berrangé <berrange@redhat.com>
2021-05-25 11:22:22 +0100

    support filesystem domain capabilities schema
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- domain_capabilities.go
+++ domain_capabilities.go
@@ -94,11 +94,12 @@ type DomainCapsEnum struct {
 }
 
 type DomainCapsDevices struct {
-	Disk     *DomainCapsDevice `xml:"disk"`
-	Graphics *DomainCapsDevice `xml:"graphics"`
-	Video    *DomainCapsDevice `xml:"video"`
-	HostDev  *DomainCapsDevice `xml:"hostdev"`
-	RNG      *DomainCapsDevice `xml:"rng"`
+	Disk       *DomainCapsDevice `xml:"disk"`
+	Graphics   *DomainCapsDevice `xml:"graphics"`
+	Video      *DomainCapsDevice `xml:"video"`
+	HostDev    *DomainCapsDevice `xml:"hostdev"`
+	RNG        *DomainCapsDevice `xml:"rng"`
+	FileSystem *DomainCapsDevice `xml:"filesystem"`
 }
 
 type DomainCapsDevice struct {
